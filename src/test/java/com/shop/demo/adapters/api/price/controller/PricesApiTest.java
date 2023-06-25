2843745ed874336289afe6ac29c40569fb4718ec
semaperez <semaperezmartin@gmail.com>
2023-06-25 18:05:16 +0200

    integration test optimized
    unnecessary test removed

--- src/test/java/com/shop/demo/adapters/api/price/controller/PricesApiTest.java
+++ src/test/java/com/shop/demo/adapters/api/price/controller/PricesApiTest.java
@@ -1,5 +1,6 @@
 package com.shop.demo.adapters.api.price.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shop.demo.adapters.api.price.dto.PriceDto;
 import jakarta.transaction.Transactional;
@@ -12,6 +13,8 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.io.UnsupportedEncodingException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -29,98 +32,53 @@ class PricesApiTest {
     @Autowired
     private ObjectMapper mapper;
 
-    @Test
-    void givenRangeDate1AndProducIdAndBrandId_when_getPrice_thenSuccessReturns() throws Exception {
-        String rangeDate = "2020-06-14T10:00:00";
-        MockHttpServletResponse response = mvc.perform(MockMvcRequestBuilders
+    private MockHttpServletResponse getResponse (String rangeDate) throws Exception {
+        return mvc.perform(MockMvcRequestBuilders
                         .get(ROOT_PATH)
                         .param("rangeDate", rangeDate)
                         .param("productId", PRODUCT_ID)
                         .param("brandId", BRAND_ID)
                         .accept(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse();
+    }
 
+    private void assertResponse(MockHttpServletResponse response, Integer priceListExpected) throws UnsupportedEncodingException,
+            JsonProcessingException {
         assertNotNull(response);
         assertEquals(HttpStatus.OK.value(), response.getStatus());
 
         PriceDto priceDtoResponse = mapper.readValue(response.getContentAsString(), PriceDto.class);
-        assertEquals(1, priceDtoResponse.getPriceList());
         assertEquals(Integer.valueOf(PRODUCT_ID), priceDtoResponse.getProductId());
+        assertEquals(priceListExpected, priceDtoResponse.getPriceList());
+    }
+
+    @Test
+    void givenRangeDate1AndProducIdAndBrandId_when_getPrice_thenSuccessReturns() throws Exception {
+        MockHttpServletResponse response = getResponse("2020-06-14T10:00:00");
+        assertResponse(response,1);
     }
 
     @Test
     void givenRangeDate2AndProducIdAndBrandId_when_getPrice_thenSuccessReturns() throws Exception {
-        String rangeDate = "2020-06-14T16:00:00";
-        MockHttpServletResponse response = mvc.perform(MockMvcRequestBuilders
-                        .get(ROOT_PATH)
-                        .param("rangeDate", rangeDate)
-                        .param("productId", PRODUCT_ID)
-                        .param("brandId", BRAND_ID)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andReturn().getResponse();
-
-        assertNotNull(response);
-        assertEquals(HttpStatus.OK.value(), response.getStatus());
-
-        PriceDto priceDtoResponse = mapper.readValue(response.getContentAsString(), PriceDto.class);
-        assertEquals(2, priceDtoResponse.getPriceList());
-        assertEquals(Integer.valueOf(PRODUCT_ID), priceDtoResponse.getProductId());
+        MockHttpServletResponse response = getResponse("2020-06-14T16:00:00");
+        assertResponse(response,2);
     }
 
     @Test
     void givenRangeDate3AndProducIdAndBrandId_when_getPrice_thenSuccessReturns() throws Exception {
-        String rangeDate = "2020-06-14T21:00:00";
-        MockHttpServletResponse response = mvc.perform(MockMvcRequestBuilders
-                        .get(ROOT_PATH)
-                        .param("rangeDate", rangeDate)
-                        .param("productId", PRODUCT_ID)
-                        .param("brandId", BRAND_ID)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andReturn().getResponse();
-
-        assertNotNull(response);
-        assertEquals(HttpStatus.OK.value(), response.getStatus());
-
-        PriceDto priceDtoResponse = mapper.readValue(response.getContentAsString(), PriceDto.class);
-        assertEquals(1, priceDtoResponse.getPriceList());
-        assertEquals(Integer.valueOf(PRODUCT_ID), priceDtoResponse.getProductId());
+        MockHttpServletResponse response = getResponse("2020-06-14T21:00:00");
+        assertResponse(response,1);
     }
 
     @Test
     void givenRangeDate4AndProducIdAndBrandId_when_getPrice_thenSuccessReturns() throws Exception {
-        String rangeDate = "2020-06-15T10:00:00";
-        MockHttpServletResponse response = mvc.perform(MockMvcRequestBuilders
-                        .get(ROOT_PATH)
-                        .param("rangeDate", rangeDate)
-                        .param("productId", PRODUCT_ID)
-                        .param("brandId", BRAND_ID)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andReturn().getResponse();
-
-        assertNotNull(response);
-        assertEquals(HttpStatus.OK.value(), response.getStatus());
-
-        PriceDto priceDtoResponse = mapper.readValue(response.getContentAsString(), PriceDto.class);
-        assertEquals(3, priceDtoResponse.getPriceList());
-        assertEquals(Integer.valueOf(PRODUCT_ID), priceDtoResponse.getProductId());
+        MockHttpServletResponse response = getResponse("2020-06-15T10:00:00");
+        assertResponse(response,3);
     }
-    
+
     @Test
     void givenRangeDate5AndProducIdAndBrandId_when_getPrice_thenSuccessReturns() throws Exception {
-        String rangeDate = "2020-06-16T21:00:00";
-        MockHttpServletResponse response = mvc.perform(MockMvcRequestBuilders
-                        .get(ROOT_PATH)
-                        .param("rangeDate", rangeDate)
-                        .param("productId", PRODUCT_ID)
-                        .param("brandId", BRAND_ID)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andReturn().getResponse();
-
-        assertNotNull(response);
-        assertEquals(HttpStatus.OK.value(), response.getStatus());
-
-        PriceDto priceDtoResponse = mapper.readValue(response.getContentAsString(), PriceDto.class);
-        assertEquals(4, priceDtoResponse.getPriceList());
-        assertEquals(Integer.valueOf(PRODUCT_ID), priceDtoResponse.getProductId());
+        MockHttpServletResponse response = getResponse("2020-06-16T21:00:00");
+        assertResponse(response,4);
     }
 }
